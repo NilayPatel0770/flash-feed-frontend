b0e8f4c84eb187ecf073db7c921733ed519f4b02
OMEN <b@>
2026-03-14 16:28:02 +0530

    feat: display news data

--- src/component/Layout/Master.jsx
+++ src/component/Layout/Master.jsx
@@ -1,13 +1,17 @@
-import { Outlet } from 'react-router-dom'
-import Header from './Header'
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Navbar from "../UI/Navbar";
 
 const Master = () => {
-    return (
-        <>
-            <Header />
-            <Outlet />
-        </>
-    )
-}
+  return (
+    <div className="bg-base">
+      <Header />
+      <div className="container mx-auto">
+        {/* <Navbar /> */}
+        <Outlet />
+      </div>
+    </div>
+  );
+};
 
-export default Master
+export default Master;
--- src/feature/SaveForLater.jsx
+++ src/feature/SaveForLater.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SaveForLater = () => {
   return (
     <div className="flex  items-center justify-center  w-10 h-10 rounded-full bg-white shadow-lg ">
-      <BookmarkIcon className="block h-4 w-4" />
+      <BookmarkIcon className="block h-4 w-4 text-black" />
     </div>
   );
 };
