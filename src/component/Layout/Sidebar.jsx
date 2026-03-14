37ac21291c466063c9af1d80eff13a2bf03982f0
OMEN <b@>
2026-03-14 17:57:51 +0530

    fix: sidebar

--- src/component/Layout/Sidebar.jsx
+++ src/component/Layout/Sidebar.jsx
@@ -25,12 +25,12 @@ const Sidebar = ({ isSidebarOpen, onCloseSidebar }) => {
     };
     return (
          // sidebar started
-        <div className={`bg-inv-bg text-primary  shadow absolute transition-all duration-300 ease-in-out top-0  w-64 h-full z-10  ${isSidebarOpen ? 'left-[0]' : 'left-[-270px]'}`} >
+        <div className={`bg-inv-bg text-primary  shadow absolute transition-all duration-300 ease-in-out top-0  w-64 h-full z-60 ${isSidebarOpen ? 'left-[0]' : 'left-[-270px]'}`} >
             {/* Your sidebar content */}
             <div className='flex flex-col h-full justify-between'>
                 <div className="">
                     <div className='flex items-center my-3'>
-                        <ChevronLeftIcon className='text-inv-text h-12 cursor-pointer' onClick={onCloseSidebar} />
+                        <ChevronLeftIcon className='text-inv-text h-6 md:h-12 cursor-pointer' onClick={onCloseSidebar} />
                         <span className='text-inv-text font-normal'>Back to Home</span>
                     </div>
                     <h2 className="text-xl font-semibold text-inv-text 50 mb-2 p-4   ">News Categories</h2>
