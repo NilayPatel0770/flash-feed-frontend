b242ab1280641dde2dcc2cec50237c2a3a454ca7
OMEN <b@>
2026-03-14 19:57:30 +0530

    fix: deploayment issue

---
+++ scripts/fetchNews.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+import fetch from "node-fetch";
+
+const category = "all_news"; // change category if needed
+const url = `https://inshorts.com/api/en/news?category=${category}&max_limit=10&include_card_data=true`;
+
+async function fetchNews() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    fs.writeFileSync(
+      "./public/news.json",
+      JSON.stringify(data, null, 2)
+    )
+  } catch (error) {
+    console.error("Error fetching news:", error);
+  }
+}
+
+fetchNews();
